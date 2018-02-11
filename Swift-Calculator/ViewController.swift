07235574d8c8b475885ccf4977d998ceb5cbf008
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-02-10 22:01:07 -0500

    Initial Commit

---
+++ Swift-Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Calculator
+//
+//  Created by Julia Gao Miller on 2/10/18.
+//  Copyright © 2018 Julia Gao Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
